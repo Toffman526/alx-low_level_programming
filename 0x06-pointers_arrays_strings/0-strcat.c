77d5dc08416cf5096a999035dd26ce4e959dd7cc
Toffman526 <fatusioluwafemi@gmail.com>
2023-09-21 19:58:28 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,10 @@ count++;
 }
 while (count2 >= 0)
 {
-*(dest + count) = *(src + count2);							
+*(dest + count) = *(src + count2);					
 if (*(src + count2) == '\0')
 break;
-count++;										
+count++;								
 count2++;
 }
 return (dest);
